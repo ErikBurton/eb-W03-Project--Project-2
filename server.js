5ddc60e106b0382e134504b3445b277d7a44ca1f
Erik Burton <erikburton1@gmail.com>
2025-05-22 20:38:48 -0600

    Updated console.log server running message

--- server.js
+++ server.js
@@ -19,5 +19,5 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => 
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`)
 );
