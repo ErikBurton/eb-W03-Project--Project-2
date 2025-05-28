4c8a152093791b28ef384e69d2fb6e6a34957a00
Erik Burton <erikburton1@gmail.com>
2025-05-27 19:02:46 -0600

    more debugging

--- auth-github.js
+++ auth-github.js
@@ -1,6 +1,9 @@
 const passport      = require('passport');
 const GitHubStratgey = require('passport-github2');
 
+// Log the callback URL at startup
+console.log('⚙️ GitHub OAuth Callback URL =', process.env.GITHUB_CALLBACK_URL);
+
 passport.serializeUser((user, done) => done(null, user));
 
 passport.deserializeUser((obj, done) => done(null, obj));
@@ -16,15 +19,17 @@ passport.use(new GitHubStratgey ({
 ));
 
 // Error handler on strategy
-passport._strategy('github')._oauth2.getOAuthAccessToken =
-function(code, params, callback) {
-  return passport._strategy('github')._oauth2.__proto__
-    .getOAuthAccessToken.call(this, code, params, (err, token, refresh, results) => {
-      if (err) {
-        console.error('GitHub token error:', err, results);
-      }
-      callback(err, token, refresh, results);
-    });
+// Monkey-patch the OAuth2 token handler to log errors
+const strategy = passport._strategy('github');
+const originalGetToken = strategy._oauth2.getOAuthAccessToken;
+strategy._oauth2.getOAuthAccessToken = function(code, params, callback) {
+  return originalGetToken.call(this, code, params, (err, token, refresh, results) => {
+    if (err) {
+      console.error('❌ GitHub token exchange error:', err);
+      console.error('❌ GitHub response payload:', results);
+    }
+    callback(err, token, refresh, results);
+  });
 };
 
 module.exports = passport;
